af96ce165d5159d77067e95a4179ed305ebb5fc7
Glitch (momentous-trick) <none>
2018-04-04 17:33:28 +0000

    👗👱 Checkpoint ./server.js:503759/304

--- server.js
+++ server.js
@@ -53,3 +53,7 @@ mongodb.MongoClient.connect((uri, err, db) => {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.
+//If I pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain an error instead.
+//When I visit that shortened URL, it will redirect me to my original link
