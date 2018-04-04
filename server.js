d007508764bb9c2e4639945b680e8ebbc4a8a383
Glitch (momentous-trick) <none>
2018-04-04 23:01:36 +0000

    🧀🦀 Checkpoint ./server.js:503759/777

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get(('/new/:toShort(*)'), (req, res, next) => {
   const valid = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
   //test if it passes the regex
   if(valid.test(toShort) === true){
-    //If it is formatted right, then we check to see if it already exists
+    //If it is formatted right, then we check to see if it already exists in the collection
     shortUrl.findOne({'originalUrl': toShort}, (err, resData) => {
       if(err){
         console.log(err);
@@ -48,49 +48,31 @@ app.get(('/new/:toShort(*)'), (req, res, next) => {
         let num = Math.floor(Math.random() * 100000).toString();
         let data = new shortUrl(
           {
-          originalUrl: toShort,
-          shortenedUrl: num,
+            originalUrl: toShort,
+            shortenedUrl: num,
           });
       data.save(err => {
         if(err){
           console.log('error saving to DB');
         } else {
-          res.send({originalUrl: toShort, shortenedUrl: num});
+          res.send({originalUrl: toShort, shortenedUrl: projectUrl + num});
         }
       });
     
     }else{
-      //console.log('this already exists');
-      //console.log(resData.originalUrl);
-      res.send({originalUrl: resData.originalUrl, shortenedUrl: resData.shortenedUrl});
+      //If resData is not null, the entry already exists. I return the data from that entry
+      res.send({originalUrl: resData.originalUrl, shortenedUrl: projectUrl + resData.shortenedUrl});
     }
        
     });
       
   }else{
+    //The url was not formatted correctly
     res.send('Failed to Shorten, please structure as a proper url.');
   }
 });
-
-
-
-
-
 
 // listen for requests :)
 var listener = app.listen('3000', function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
-
-
-//I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.
-//If I pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain an error instead.
-//When I visit that shortened URL, it will redirect me to my original link
-
-//is url valid?
-//has url been entered before?
-//search for url
-
-//app.get for initial entry of url to be shortened
-//app.get for new website they are going to
